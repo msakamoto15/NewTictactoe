78f3225e2790056d6f4545361bf26f2a3d2cda71
msakamoto15 <msakamoto15@punahou.edu>
2015-04-03 17:12:37 -1000

    New Log
    New Log

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 public class TicTacToe {
   
   public static void main(String[] args){
     GameBoard myGame = new GameBoard();
-    myGame.displayBoard();
-   
+  //  myGame.displayBoard();
+    Scanner scan = new Scanner(System.in);
+    boolean keepPlaying = true;
     
+    do{
+       myGame.displayBoard();
+//
+       System.out.println("Choose the number that represents the option you want.");
+       scan.nextInt();
+      
+      
+    }while(keepPlaying == false);//change to true
   }
 }
