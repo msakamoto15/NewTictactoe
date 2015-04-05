f91b4f4bfb011db2775a9c13fb2994fd33bd085b
msakamoto15 <msakamoto15@punahou.edu>
2015-04-05 11:46:30 -1000

    new sync
    new sync

--- TicTacToe.java
+++ TicTacToe.java
@@ -4,6 +4,7 @@ public class TicTacToe {
   public static void main(String[] args){
     GameBoard myGame = new GameBoard();
   //  myGame.displayBoard();
+    int playermove;
     Scanner scan = new Scanner(System.in);
     boolean keepPlaying = true;
     
@@ -12,6 +13,18 @@ public class TicTacToe {
 //
        System.out.println("Choose the number that represents the option you want.");
        scan.nextInt();
+       playermove = scan.nextInt();
+       if(playermove == 1){
+         grid[0][0] = 'X';
+       }
+       if(playermove == 2){
+         grid[0][2] = 'X';
+       }
+       if(playermove == 3){
+         grid[0][4] = 'X';
+       }
+       
+        playermove = scan.nextInt();
       
       
     }while(keepPlaying == false);//change to true
